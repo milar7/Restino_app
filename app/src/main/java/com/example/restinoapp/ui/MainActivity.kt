d75b0bf9909290a692ed69822543a357feaa6d69
milar7 <miladir@yahoo.com>
2020-05-23 21:24:31 +0430

    add navigation dependency and impeliment it add home fragment

--- app/src/main/java/com/example/restinoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/restinoapp/ui/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
     }
 
 
